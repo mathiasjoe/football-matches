60c545d6594a5067abb3d712546207defcbe2926
DESKTOP-Q9OG6AL\Mathias <mathijo@stud.ntnu.no>
2022-02-01 12:14:31 +0100

    mapped Matches in a table

--- src/Matches.js
+++ src/Matches.js
@@ -1,21 +1,56 @@
 import dataStore from "./dataStore";
-import React, { Component, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Table } from "semantic-ui-react";
-import Loading from "./Loading";
+import './Matches.css'
 
 const Matches = (props) => {
-  const [matches, setMatches] = [];
+  const [matches, setMatches] = React.useState([]);
 
   useEffect(() => {
     dataStore.matches().then((res) => {
-      console.log(res.matches);
-      setMatches(res.matches);
+      console.log(res.data.matches);
+      setMatches(res.data.matches);
     });
   }, []);
 
   return (
     <div className="matches">
-      <h1>hei</h1>
+      {matches.map(match => {
+        return (
+            <Table>
+              <thead>
+              <tr>
+                <th>Competition name</th>
+                <th>Matchday</th>
+                <th>Home Team</th>
+                <th>Away Team</th>
+                <th>Start time</th>
+                <th>Home Score</th>
+                <th>Away Score</th>
+
+
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                <td>{match.competition.name}</td>
+                <td>{match.matchday}</td>
+                <td>{match.homeTeam.name}</td>
+                <td>{match.awayTeam.name}</td>
+                <td>{match.utcDate}</td>
+                {(match.score.fullTime.homeTeam == null ?
+                        (<td>-</td>)
+                        : (<td>{match.score.fullTime.homeTeam}</td>)
+                )}
+                {(match.score.fullTime.awayTeam == null ?
+                        (<td>-</td>)
+                        : (<td>{match.score.fullTime.awayTeam}</td>)
+                )}
+              </tr>
+              </tbody>
+            </Table>
+        );
+      })}
     </div>
   );
 };
